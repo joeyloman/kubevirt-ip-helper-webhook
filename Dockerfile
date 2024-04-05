d4e48652f13ccc6ddef02f799754f82078eadb57
Joey Loman <joey@binbash.org>
2024-04-05 09:51:55 +0200

    - Bumped the golang version to 1.22 - Bumped the container build golang version to 1.22 - Bumped the alpine container to version 3.19 - Updated all vendor modules to the latest versions

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM docker.io/golang:1.20-alpine3.17 as builder
+FROM docker.io/golang:1.22-alpine3.19 as builder
 RUN mkdir /src /deps
 RUN apk update && apk add git build-base binutils-gold
 WORKDIR /deps
@@ -7,7 +7,7 @@ RUN go mod download
 ADD / /src
 WORKDIR /src
 RUN go build -o kubevirt-ip-helper-webhook .
-FROM docker.io/alpine:3.17
+FROM docker.io/alpine:3.19
 RUN adduser -S -D -h /app kubevirt-ip-helper-webhook
 USER kubevirt-ip-helper-webhook
 COPY --from=builder /src/kubevirt-ip-helper-webhook /app/
--- vendor/github.com/joeyloman/kubevirt-ip-helper/pkg/apis/kubevirtiphelper.k8s.binbash.org/v1/types.go
+++ vendor/github.com/joeyloman/kubevirt-ip-helper/pkg/apis/kubevirtiphelper.k8s.binbash.org/v1/types.go
@@ -60,8 +60,9 @@ type IPPool struct {
 }
 
 type IPPoolSpec struct {
-	IPv4Config  IPv4Config `json:"ipv4config,omitempty"`
-	NetworkName string     `json:"networkname,omitempty"`
+	IPv4Config    IPv4Config `json:"ipv4config,omitempty"`
+	NetworkName   string     `json:"networkname,omitempty"`
+	BindInterface string     `json:"bindinterface,omitempty"`
 }
 
 type IPv4Config struct {
@@ -72,6 +73,7 @@ type IPv4Config struct {
 	DNS          []string `json:"dns,omitempty"`
 	DomainName   string   `json:"domainname,omitempty"`
 	DomainSearch []string `json:"domainsearch,omitempty"`
+	NTP          []string `json:"ntp,omitempty"`
 	LeaseTime    int      `json:"leasetime,omitempty"`
 }
 
